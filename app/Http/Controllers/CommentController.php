d20b3afbfb48289762a6616e57839a69f34d0b4d
meivaldi <meivaldi@gmail.com>
2020-07-07 08:52:29 +0700

    Simple API for comment done

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,8 +3,40 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Comment;
 
 class CommentController extends Controller
 {
-    //
+    public function create(Request $request, $post_id) {
+        $comment = new Comment;
+        $comment->user_id = Auth::id();
+        $comment->post_id = $post_id;
+        $comment->comment = $request->comment;
+        $comment->save();
+
+        return response()->json([
+            'error' => false,
+            'status' => 'Berhasil disimpan'
+        ]);
+    }
+
+    public function find($post_id) {
+        $comment = Comment::find($post_id);
+
+        return response()->json([
+            'error' => false,
+            'status' => 'Berhasil',
+            'data' => $comment
+        ]);
+    }
+
+    public function delete($post_id) {
+        $comment = Comment::find($post_id);
+        $comment->delete();
+
+        return response()->json([
+            'error' => false,
+            'status' => 'Berhasil dihapus'
+        ]);
+    }
 }
